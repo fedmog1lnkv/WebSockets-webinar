a4aca41bc20f47e7c904e76f53ad3de12f80d009
Fedor Kuznetsov <kuzneczov-f@susanoo.ru>
2023-05-30 21:58:37 +0800

    Mini client
    The client implements sending messages and clearing the chat

--- WSServer/Program.cs
+++ WSServer/Program.cs
@@ -28,4 +28,11 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+app.UseCors(options =>
+{
+    options.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+});
+
 app.Run();
