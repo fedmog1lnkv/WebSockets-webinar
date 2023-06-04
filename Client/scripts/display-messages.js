823bad3309190eb038dfdbf31f10d86d0c6e07ec
Fedor Kuznetsov <kuzneczov-f@susanoo.ru>
2023-06-04 17:16:09 +0800

    Add more comments

--- Client/scripts/display-messages.js
+++ Client/scripts/display-messages.js
@@ -1,27 +1,29 @@
 function displayMessage(message) {
-    // Если получено сообщение "@$/clear", то очищаем чат
+    // If the message "@$/clear" is received, clear the chat
     if (message === "@$/clear") {
         document.getElementById("messages").innerHTML = "";
         return;
     }
 
-    // Парсим имя пользователя и сообщение из полученного сообщения
-    var separatorIndex = message.indexOf(':');
+    // Parse username and message from received message
+    var separatorIndex = message.indexOf(":");
     var username = message.substring(0, separatorIndex).trim();
     var messageText = message.substring(separatorIndex + 1).trim();
 
-    // Если в localstorage есть имя пользователя и оно совпадает с именем в сообщении, добавляем класс "self" к сообщению
-    var selfClass = '';
-    var alignName = 'name-left';
-    var storedUsername = localStorage.getItem('username');
+    // If the username exists in localStorage and matches the one in the message, add "self" class to the message
+    var selfClass = "";
+    var alignName = "name-left";
+    var storedUsername = localStorage.getItem("username");
+
     if (storedUsername && username === storedUsername) {
-        selfClass = ' darker';
-        alignName = 'name-right';
+        selfClass = " darker";
+        alignName = "name-right";
     }
 
+    // Append the message to the messages container
     var messagesDiv = document.getElementById("messages");
     messagesDiv.innerHTML += '<div class="container' + selfClass + '">' +
-        '<p>' + messageText + '<p>' +
-        '<span class="' + alignName + '">' + username + '</span>' +
-        '</div>';
+        '<p>' + messageText + '</p>' +
+        '<span class="' + alignName + '">' + username + "</span>" +
+        "</div>";
 }
--- Client/scripts/main.js
+++ Client/scripts/main.js
@@ -1,43 +1,51 @@
-// Открываем всплывающее окно с формой ввода имени
+// Open a pop-up window with the username input form when the page loads if the username doesn't exist in localStorage
 window.onload = function () {
-    if (!localStorage.getItem('username')) {
+    if (!localStorage.getItem("username")) {
         var username = prompt("Please enter your name:");
+
+        // Prompt user to enter their name until a valid input has been provided
         while (username === null || username === "") {
             username = prompt("Please enter your name:");
         }
+
         setUsername(username);
     }
-}
+};
 
+// Save the provided username to localStorage
 function setUsername(username) {
-    localStorage.setItem('username', username);
+    localStorage.setItem("username", username);
 }
 
-// Форма ввода сообщения
+// Message input form event listener
 var form = document.getElementById("send-form");
 
 form.addEventListener("submit", function (event) {
     event.preventDefault();
+
+    // Get the message input and username from localStorage
     var input = document.getElementById("message-input");
     var message = input.value;
-    var username = localStorage.getItem('username'); // получаем имя пользователя из localstorage
+    var username = localStorage.getItem("username");
+
+    // Send the message if a user exists and a message has been entered
     if (username !== null || username !== "") {
         if (message) {
-            socket.send(username + ': ' + message); // отправляем сообщение со значением из localstorage
+            socket.send(username + ": " + message);
             input.value = "";
         }
     }
 });
 
-// HTTP Delete на сервер для очистки чата
+// HTTP DELETE request to clear the chat on the server
 function clearChat() {
     var requestOptions = {
-        method: 'DELETE',
-        redirect: 'follow'
+        method: "DELETE",
+        redirect: "follow",
     };
 
     fetch("http://158.160.26.1:8080/chat/clear", requestOptions)
-        .then(response => response.text())
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error));
+        .then((response) => response.text())
+        .then((result) => console.log(result))
+        .catch((error) => console.log("error", error));
 }
